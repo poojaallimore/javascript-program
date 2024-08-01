2acc9f38314105fc06909794e1f46b7378388092
poojaallimore <poojallimore.chetan@gmail.com>
2024-08-01 15:34:25 +0530

    javascript programm
    Javascript program

---
+++ 01-variableAssign.js
@@ -0,0 +1,50 @@
+console.log("====================== Step 1================================");
+
+var myFirstName;
+
+console.log("1.My First Name is:",myFirstName);
+
+var myFirstName="Pooja Yadavannavar";
+
+console.log("2.My First and Last Name is:",myFirstName);
+
+var myFirstName="Pooja Chetan Yadavannavar";
+
+console.log("3.My Full Name is:",myFirstName);
+
+console.log("===================== Step 2================================");
+
+var myLove="I Love Only JS";
+
+console.log("2.My Love is:",myLove);
+
+console.log("======================= Step 3=================================");
+
+var firstName="Pooja";
+
+console.log("3.1 My First Name is:",firstName);
+
+var lastName="Chetan";
+
+console.log("3.2 My Last Name is:",lastName);
+
+var age=35;
+
+console.log("3.2 My Age is:",age);
+
+var address="pune";
+
+console.log("3.3 My Address:",address);
+
+var pinCode=410501;
+
+console.log("3.5 PinCode:",pinCode);
+
+var gender="female";
+
+console.log("3.6 Gender:",gender);
+
+var isMarried=true;
+
+console.log("3.7 Are you married:",isMarried);
+
---
+++ 02-typeOf.js
@@ -0,0 +1,54 @@
+console.log("================Step 1=========================");
+ var bankName;
+
+ console.log("1 Bank Name is:",bankName);
+
+ var bankNameTypeOf=typeof bankName;
+
+ console.log("2 Type Of bankName is:",bankName);
+
+ console.log("================Step 2=========================");
+
+ var marks='90%';
+
+ console.log("1 Marks is:",marks);
+
+ var marksTypeOf=typeof marks;
+
+ console.log("2 Type of marks is:",marksTypeOf);
+
+ console.log("==================Step 3=========================");
+
+ var isWorking;
+
+ console.log(" 1.1 Is Working:",isWorking);
+
+ var isWorkingTypeOf=typeof isWorking;
+
+ console.log(" 1.2 Type of isWorking is:",isWorkingTypeOf);
+
+ var isWorking=true;
+
+ console.log(" 2.1 Are you working:",isWorking);
+
+  var isWorkingTypeOf=typeof isWorking;
+
+  console.log(" 2.2 Type Of isWorking is:",isWorkingTypeOf);
+
+  console.log("================Step 4==============================");
+
+  var totalCount="Hundred and Seven";
+
+  console.log("1.1 Total Count is:",totalCount);
+
+  var totalCountTypeOf= typeof totalCount;
+
+  console.log("1.2 Type Of Total Count is:",totalCountTypeOf);
+
+  var totalCount=107;
+
+  console.log("2.1 Total Count is:",totalCount);
+
+  var totalCountTypeOf=typeof totalCount;
+
+  console.log("2.2 Type Of totalCount is:",totalCountTypeOf);
