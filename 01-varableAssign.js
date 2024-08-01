9fe58c2d3aaaa7f3c8e4cf10eab201c74611eaaf
poojaallimore <poojallimore.chetan@gmail.com>
2024-08-01 13:07:40 +0530

    javascript variables
    javascript variables

---
+++ 01-varableAssign.js
@@ -0,0 +1,9 @@
+console.log("1. My City Name is:Pune");
+
+console.log("2. My Best friend Name is:Varsha");
+
+console.log("3. My Country Name is:India");
+
+console.log("4. My School Name is:SMVVP");
+
+console.log("5. My Collage Name is:SNDCOE");
---
+++ 01-variable.js
@@ -0,0 +1,16 @@
+
+// variable declaration
+var fistName;
+console.log(fistName);
+
+// variable initialization
+fistName="Jenny";
+console.log(fistName);
+
+// variable Declaration and Initialization
+var lastName="Gates";
+console.log(lastName);
+
+// variable update
+fistName="Jennifer";
+console.log(fistName);
