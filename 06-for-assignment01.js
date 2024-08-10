86598bd17c85a0886f0e0f1a0f147898b9468a3f
poojaallimore <poojallimore.chetan@gmail.com>
2024-08-10 07:27:42 +0530

    id-06:forLoopAssignment
    id-06:forLoopAssignment

---
+++ 06-for-assignment01.js
@@ -0,0 +1,60 @@
+
+console.log(`======= 1.WAP to print number from 5 to 15 by incrementing 1========= `);
+
+for (var i = 5 ; i <= 15 ; i++) {
+    
+    console.log(i);
+       
+}
+
+console.log(`===== 2.WAP to print numbers from 50 to 40 decrementing by 1===========`);
+
+for (var i= 50 ; i >= 40 ; i--) {
+  
+    console.log(i);
+    
+}
+
+console.log(`==== 3.WAP to find first 15 odd numbers====================================`);
+
+for (var i = 1 ; i <= 31 ; i=i+2) {
+    
+    console.log(i);
+       
+}
+
+console.log(`==== 4.WAP to find first 10 even number========================================`);
+
+for (var i = 0 ; i <= 20 ; i=i+2) {
+   
+    console.log(i);
+    
+}
+
+console.log(`=== 5.WAP to print table of 5 ==================================================`);
+
+for (var i = 5 ; i <= 50 ; i = i+5) {
+    
+    console.log(i);
+    
+}
+
+console.log(`==== 6.WAP to print table of 10 =================================================`);
+ 
+for (var i = 10 ; i <= 100 ; i = i+10) {
+   
+    console.log(i);
+     
+ }
+
+ console.log(`=== 7.WAP to print table of 10 in reverse order ==================================`);
+ 
+ for (var i = 100 ; i >= 10; i = i-10) {
+    
+    console.log(i);
+    
+ }
+
+
+
+
