2ab02d789defa02dfbac9a4488f25b876441e823
poojaallimore <poojallimore.chetan@gmail.com>
2024-08-22 16:40:41 +0530

    id:09string-assignment03
    id:09string-assignment03

---
+++ 09-string-assign03.js
@@ -0,0 +1,21 @@
+
+console.log(`===== WAP to count word=======================================`);
+
+function wordCount(strng) {
+
+     console.log("Given string is:",strng);
+
+     let word = strng.split(" ");
+
+    console.log(word);
+
+    console.log("Total number of word in given string is", word.length);
+    
+
+    
+    
+      
+     
+}
+
+wordCount("JavaScript The language of Internet");
