b47ef4670556e5b93c23d1523d99309dc82d52ce
poojaallimore <poojallimore.chetan@gmail.com>
2024-08-22 16:41:57 +0530

    Id:09-string-assignment
    Id:09-string-assignment

--- 09-string-assignment.js
+++ 09-string-assignment.js
@@ -3,6 +3,9 @@ console.log(`==== 1.WAF to count character "a" and "A"==========================
 
 function charCount(strng) {
 
+    console.log(`Given string is:${strng}`);
+    
+
     let count = 0;
 
     for (let index = 0; index < strng.length; index++) {
@@ -18,20 +21,20 @@ function charCount(strng) {
 
 let result = charCount("JavaScript");
 
-console.log(`1.Total count of "a" and "A" in given string "JavaScript" is:${result}`);
+console.log(`1.Total count of "a" and "A" in given string is:${result}`);
 
 let result1 = charCount("Do or Die"); 
 
-console.log(`2.Total count of "a" and "A" in given string "Do or Die" sis:${result1}`);
+console.log(`2.Total count of "a" and "A" in given string is:${result1}`);
  
 let result3 = charCount("Learn with us, Job with us"); 
 
-console.log(`3.Total count of "a" and "A" in given string "Learn with us, Job with us" is:${result3}`);
+console.log(`3.Total count of "a" and "A" in given string is:${result3}`);
 
 let result4 = charCount("Empowering Dreams, Guaranteeing Futures"); 
 
-console.log(`4.Total count of "a" and "A" in given string "Empowering Dreams, Guaranteeing Futures": ${result4}`);
+console.log(`4.Total count of "a" and "A" in given string is: ${result4}`);
 
 let result5 = charCount("Anny, My favorite fruit is Apple");
 
-console.log(`5.Total count of "a" and "A" in given string "Anny, My favorite fruit is Apple:" ${result5}`);
+console.log(`5.Total count of "a" and "A" in given string : ${result5}`);
