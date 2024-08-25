6dd3517cf2bb922fad7e5f8874f490ec99088b9c
poojaallimore <poojallimore.chetan@gmail.com>
2024-08-25 17:57:36 +0530

    Update 06-array-remove-duplicate.js

--- 06-array-remove-duplicate.js
+++ 06-array-remove-duplicate.js
@@ -1,44 +1,77 @@
+
+console.log(`======WAP to remove duplicate element from array========`);
+
+let arrayNum = [11,3,4,11,4,7,3];
  
-//  let greet = "How are you mate";
+console.log(`Given array is:${arrayNum}`);
+ 
+let updatedArray = [];
 
-//  for (let index = 0; index < greet.length; index++) {
-    
-//     let char = greet.charAt(index);
+for (let i =0; i < arrayNum.length; i++) {
+  
+   if (updatedArray.indexOf(arrayNum[i]) === -1) {
+       
+      updatedArray.push(arrayNum[i]);
+   }
+   
+}
+console.log(`After removing duplicate element:${updatedArray}`);
 
-//     if (index%2 == 0) {
-        
-//         const element = greet[index];
-        
-//         const upperCase = element.toUpperCase();
-         
-//         console.log(upperCase);
-        
 
+
+// console.log(`==== WAP to make string char in  alternative case====================== `);
+
+// function alternativeCase(string) {
+     
+//     let newString = [];
+
+//     for (let i = 0; i < string.length; i++) {
+
+//          if (i % 2 == 0 ) {
+             
+//             newString[i] = string[i].toUpperCase();
+//          }
+//          else{
+
+//             newString[i] = string[i].toLowerCase();
+//          }
+        
 //     }
-    
+//   return newString.join(" ");
+
 //  }
 
-function alternativeCase(string) {
-     
-    let newString = [];
+// let result = alternativeCase("How are you mate");
 
-    for (let i = 0; i < string.length; i++) {
+// console.log(result);
 
-         if (i % 2 == 0 ) {
-             
-            newString[i] = string[i].toUpperCase();
-         }
-         else{
 
-            newString[i] = string[i].toLowerCase();
-         }
-        
-    }
-  return newString.join(" ");
+console.log(`==== WAP to make first and last char of string in capital case======`);
 
- }
+let string = "How are you mate";
 
-let result = alternativeCase("How are you mate");
+console.log(`Given string is:${string}`);
 
-console.log(result);
+
+let word = string.split(" ");
+
+let capitalizeWord = [];
+
+for (let index = 0; index < word.length; index++) {
+   
+   let words = word[index];
+
+   let firstLetter = words[0].toUpperCase();
+
+   const latsLetter = words[words.length-1].toUpperCase();
+
+   const middleLetter = words.slice(1,words.length-1);
+
+   capitalizeWord.push(firstLetter+middleLetter+latsLetter);
+
+   
+}
+const upperCaseStrng = capitalizeWord.join(" ");
+
+console.log(`Updated string with first letter and last letter in upper case:${upperCaseStrng}`);
 
